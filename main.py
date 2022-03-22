90bc989fbeacf3148c993dff03a6d9807cb59182
Mateo Cañavera <mcanavera@unal.edu.co>
2022-03-22 16:15:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from shutil import RegistryError
+from turtle import color
+
+
+class Asiento():
+    pass
+    def __init__(self,color,precio,registro):
+        self.color= color
+        self.precio= precio
+        self.registro= registro
+    def cambiarColor(self,color):
+        if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
+            self.color=color
+
+class Auto():
+    pass
+    def __init__(self,modelo,precio,asientos,marca,motor,registro,cantidadCreados):
+        self.modelo=modelo
+        self.precio= precio
+        self.asientos= list(asientos)
+        self.marca= marca
+        self.motor= motor
+        self.registro= registro
+
+    def cantidadAsientos(self):
+        contadorasientos=0
+        if None==self.asientos:
+            for i in self.asientos:
+                if i != None:
+                    contadorasientos+=1
+
+        return contadorasientos
+    
+    def verificarIntegridad(self):
+        mensaje1="Auto original"
+        mensaje2="Las piezas no son originales"
+        if self.registro == self.asientos[0].registro:
+            if self.motor.registro == self.registro:
+                print(mensaje1)
+            else:
+                print(mensaje2)
+        else:
+            print(mensaje2)
+
+class Motor():
+    pass
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
